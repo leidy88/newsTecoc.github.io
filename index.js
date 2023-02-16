658b4c72186bf26ad5bf68e6576fa9a74414dd3e
leidy88 <lemabo88@gmail.com>
2023-02-16 17:52:48 -0500

    correccion

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-const hamburguer=document.querySelector('#hamburguer');
+const hamburger=document.querySelector('#hamburguer');
 const close=document.querySelector('#close');
-const menu=document.querySelector('menu');
+const menu=document.querySelector('#menu');
 
-hamburguer.addEventListener('click', () => {
-    hamburguer.getElementsByClassName.display='none';
+hamburger.addEventListener('click', () => {
+    hamburger.getElementsByClassName.display='none';
     menu.style.display='block';
     close.style.display='block';
 });
 
 close.addEventListener('click', () => {
-    hamburguer.getElementsByClassName.display='block';
+    hamburger.getElementsByClassName.display='block';
     menu.style.display='none';
     close.style.display='none';
 });
